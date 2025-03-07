917daadf4ff9e89b1178ec55bc2eb9a42767773f
0xHexecutioner <116597049+0xHexecutioner@users.noreply.github.com>
2025-03-07 22:22:16 +0100

    Add files via upload

---
+++ Homework10.java
@@ -0,0 +1,15 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {       
+      int h = 5;
+          
+      for (int i = 1; i <= 10; i++){
+    	 int resul = h * i;
+    	  System.out.println(h + "*" + i + "=" + resul);
+      }
+      
+    }
+}
